9f9b6187f86cc43426f396bd87eb119ef5ffecff
AnkitVartak1993 <ankitvartak@gmail.com>
2018-02-11 20:10:25 -0500

    Added broadcast for new user

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ app.use(express.static(publicPath));
 io.on("connection",(socket)=>{
     console.log("New user connected");
 
+    socket.emit('newMessage',{
+        from:'Admin',
+        text:'Welcome to chat room',
+        createdAt: new Date().getDate()
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'New User Joined',
+        createdAt: new Date().getDate()
+    })
     socket.on('createMessage', (msg)=>{
     console.log(msg);
 
@@ -23,7 +34,7 @@ io.on("connection",(socket)=>{
         from: msg.from,
         text: msg.text,
         createdAt: new Date().getDate()
-    })
+    });
     });
 
    socket.on('disconnect', ()=>{
