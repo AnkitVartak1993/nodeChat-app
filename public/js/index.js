95d32124c1a81a50ba5f8a4a08c675925ad613a6
AnkitVartak1993 <ankitvartak@gmail.com>
2018-03-04 13:26:08 -0500

    Added geolocation support using geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -26,3 +26,28 @@ jQuery('#message_form').on('submit', function(e){
 
   });
 });
+
+var locationButton = jQuery('#send_location');
+locationButton.on('click', function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supported by this browser');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position){
+        socket.emit('createLocationMessage',{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function(){
+        alert('Unable to fetch location.')
+    });
+});
+
+socket.on('newLocationMessage',(msg)=>{
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+    li.text(`${msg.from}: `);
+    a.attr('href', msg.url);
+    li.append(a);
+    jQuery("#messages").append(li);
+});
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
 
+const {generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -36,7 +37,11 @@ io.on("connection",(socket)=>{
     callback('msg received');
     });
 
-   socket.on('disconnect', ()=>{
+    socket.on('createLocationMessage', (coords)=>{
+        io.emit('newLocationMessage', generateLocationMessage ('Admin',coords.latitude,coords.longitude));
+    });
+
+    socket.on('disconnect', ()=>{
         console.log("Disconnected from server");
     });
     
